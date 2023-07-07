91188672cb7dd429260c5c726673c774919eee8c
Idealisoa04 <119027567+Idealisoa04@users.noreply.github.com>
2023-07-07 21:18:58 +0300

    sprint9

---
+++ ETU1848/framework/src/Mapping.java
@@ -0,0 +1,33 @@
+package etu1848.framework;
+public class Mapping {
+    String className;
+    String method;
+    Class[] arguments;
+    public Mapping(String className, String method, Class[] arguments) {
+        this.className = className;
+        this.method = method;
+        this.arguments = arguments;
+    }
+    public Mapping(String className, String method) {
+        this.className = className;
+        this.method = method;
+    }
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    public Class[] getArguments() {
+        return arguments;
+    }
+    public void setArguments(Class[] arguments) {
+        this.arguments = arguments;
+    } 
+}
