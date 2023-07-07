72f867e2820ddbd9a7a632e9f1891fbf5df9c790
Idealisoa04 <119027567+Idealisoa04@users.noreply.github.com>
2023-07-07 19:36:36 +0300

    sprint3

---
+++ ETU1848/WEB-INF/classes/Url.java
@@ -0,0 +1,11 @@
+package etu1848.framework;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Url {
+    String method() default "your method is empty";
+}
